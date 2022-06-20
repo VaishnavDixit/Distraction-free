b875741e1db1730577f6ed2013d642e8122a71a2
Vaishnav <vaishnavdixit98@gmail.com>
2022-06-20 19:20:00 +0530

    added custom checkboxes

--- js/content.js
+++ js/content.js
@@ -4,49 +4,49 @@ var com = document.getElementById("com_check");
 
 var recVal, relVal, comVal;
 chrome.storage.sync.get(['recValue', 'relValue', 'comValue'], function (result) {
-    rec.checked = result.recValue === null ? false : result.recValue;
-    rel.checked = result.relValue === null ? false : result.relValue;
-    com.checked = result.comValue === null ? false : result.comValue;
+	rec.checked = result.recValue === null ? false : result.recValue;
+	rel.checked = result.relValue === null ? false : result.relValue;
+	com.checked = result.comValue === null ? false : result.comValue;
 });
 
 rec.addEventListener('change', function () {
-    if (rec.checked) {
-        recVal = true;
-        chrome.storage.sync.set({
-            'recValue': true
-        });
-    } else if (rec.checked === false) {
-        recVal = false;
-        chrome.storage.sync.set({
-            'recValue': false
-        });
-    }
+	if (rec.checked) {
+		recVal = true;
+		chrome.storage.sync.set({
+			'recValue': true
+		});
+	} else if (rec.checked === false) {
+		recVal = false;
+		chrome.storage.sync.set({
+			'recValue': false
+		});
+	}
 });
 
 com.addEventListener('change', function () {
-    if (com.checked) {
-        comVal = true;
-        chrome.storage.sync.set({
-            'comValue': true
-        });
-    } else if (com.checked === false) {
-        comVal = false;
-        chrome.storage.sync.set({
-            'comValue': false
-        });
-    }
+	if (com.checked) {
+		comVal = true;
+		chrome.storage.sync.set({
+			'comValue': true
+		});
+	} else if (com.checked === false) {
+		comVal = false;
+		chrome.storage.sync.set({
+			'comValue': false
+		});
+	}
 });
 
 rel.addEventListener('change', function () {
-    if (rel.checked) {
-        relVal = true;
-        chrome.storage.sync.set({
-            'relValue': true
-        });
-    } else if (rel.checked === false) {
-        relVal = false;
-        chrome.storage.sync.set({
-            'relValue': false
-        });
-    }
+	if (rel.checked) {
+		relVal = true;
+		chrome.storage.sync.set({
+			'relValue': true
+		});
+	} else if (rel.checked === false) {
+		relVal = false;
+		chrome.storage.sync.set({
+			'relValue': false
+		});
+	}
 });
